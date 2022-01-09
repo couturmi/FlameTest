a32b21a0d9a062d9164ddaa9ca8893d743b735dd
Mitchell John Couturier <couturmi@mail.gvsu.edu>
2022-01-09 11:06:08 -0500

    other test with layoutbuilder

--- flame_test/lib/main.dart
+++ flame_test/lib/main.dart
@@ -2,27 +2,49 @@ import 'package:flame/components.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 
-class MyGame extends FlameGame {
-  String text = "";
-  @override
-  Future<void> onLoad() async {
-    text += "onLoad occurred\n";
-    await super.onLoad();
-    add(TextComponent(text: text, position: Vector2(50, 50)));
-  }
+// class MyGame extends FlameGame {
+//   String text = "";
+//   @override
+//   Future<void> onLoad() async {
+//     text += "onLoad occurred\n";
+//     await super.onLoad();
+//     add(TextComponent(text: text, position: Vector2(50, 50)));
+//   }
+//
+//   @override
+//   void onGameResize(Vector2 canvasSize) {
+//     text += "onGameResize occurred\n\tCanvas Size: $canvasSize\n";
+//     super.onGameResize(canvasSize);
+//   }
+// }
+//
+// main() {
+//   final myGame = MyGame();
+//   runApp(
+//     GameWidget(
+//       game: myGame,
+//     ),
+//   );
+// }
 
-  @override
-  void onGameResize(Vector2 canvasSize) {
-    text += "onGameResize occurred\n\tCanvas Size: $canvasSize\n";
-    super.onGameResize(canvasSize);
-  }
+void main() {
+  runApp(const MyApp());
 }
 
-main() {
-  final myGame = MyGame();
-  runApp(
-    GameWidget(
-      game: myGame,
-    ),
-  );
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Welcome to Flutter',
+      home: Scaffold(
+        body: LayoutBuilder(
+          builder: (_, BoxConstraints constraints) {
+            return Center(child: Text("Constraints: ${constraints.biggest}"));
+          },
+        ),
+      ),
+    );
+  }
 }

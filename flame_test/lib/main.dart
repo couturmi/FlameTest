62c7c4755813a12f58cc205106ce46001362d9b3
Mitchell John Couturier <couturmi@mail.gvsu.edu>
2022-01-09 12:02:23 -0500

    fdsaf

--- flame_test/lib/main.dart
+++ flame_test/lib/main.dart
@@ -2,49 +2,48 @@ import 'package:flame/components.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 
-// class MyGame extends FlameGame {
-//   String text = "";
-//   @override
-//   Future<void> onLoad() async {
-//     text += "onLoad occurred\n";
-//     await super.onLoad();
-//     add(TextComponent(text: text, position: Vector2(50, 50)));
-//   }
-//
-//   @override
-//   void onGameResize(Vector2 canvasSize) {
-//     text += "onGameResize occurred\n\tCanvas Size: $canvasSize\n";
-//     super.onGameResize(canvasSize);
-//   }
-// }
-//
-// main() {
-//   final myGame = MyGame();
-//   runApp(
-//     GameWidget(
-//       game: myGame,
-//     ),
-//   );
-// }
-
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MyGame extends FlameGame {
+  String text = "";
+  @override
+  Future<void> onLoad() async {
+    text += "onLoad occurred\n";
+    await super.onLoad();
+    add(TextComponent(text: text, position: Vector2(50, 50)));
+  }
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        body: LayoutBuilder(
-          builder: (_, BoxConstraints constraints) {
-            return Center(child: Text("Constraints: ${constraints.biggest}"));
-          },
-        ),
-      ),
-    );
+  void onGameResize(Vector2 canvasSize) {
+    text += "onGameResize occurred\n\tCanvas Size: $canvasSize\n";
+    super.onGameResize(canvasSize);
   }
 }
+
+main() {
+  final myGame = MyGame();
+  runApp(
+    GameWidget(
+      game: myGame,
+      autofocus: false,
+    ),
+  );
+}
+
+// void main() {
+//   runApp(const MyApp());
+// }
+//
+// class MyApp extends StatelessWidget {
+//   const MyApp({Key? key}) : super(key: key);
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Directionality(
+//       textDirection: TextDirection.ltr,
+//       child: LayoutBuilder(
+//         builder: (_, BoxConstraints constraints) {
+//           return Center(child: Text("Constraints: ${constraints.biggest}"));
+//         },
+//       ),
+//     );
+//   }
+// }
